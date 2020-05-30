688aef2afec628651c842e9350c2c247eb291802
bescond mathis <mathisbescond@outlook.fr>
2020-05-30 14:39:51 +0200

    zxcv

---
+++ sous pages/user.js
@@ -0,0 +1,22 @@
+function myFunction() {
+  var txt;
+  var mot = prompt("Mot de passe:", "");
+  if (mot == "oui") {
+    document.location.href="sous pages/secret.html";
+  }
+  
+}
+
+var win; 
+      function openW() { 
+        if (win==null) {
+         win = window.open("image/fond.mp3", "", "top=800,left=0,height=10,width=300"); 
+      }
+    }
+       function openW2() { 
+        if (win==null) {
+         win = window.open("sous pages/image/fond.mp3", "", "top=800,left=0,height=10,width=300"); 
+      }
+    }
+
+    
